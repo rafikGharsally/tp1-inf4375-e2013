4f4f3d491c9487efe5e2794bddaf26e9db2d3ce5
Fred Prieur <fprieur@gmail.com>
2013-05-30 11:15:47 -0400

    j'ai refait le repo pour travailler avec l'outil graphique de github

---
+++ tp1-inf4375/routes/index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index');
+};
+
+/*
+ * GET test page.
+ */
+
+exports.test = function(req, res){
+  res.render('test');
+};
