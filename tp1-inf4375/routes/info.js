f959137ab76cd1e13692e67c3a83aa78b4fc042b
Fred Prieur <fprieur@gmail.com>
2013-06-03 17:11:27 -0400

    test 2 service info

--- tp1-inf4375/routes/info.js
+++ tp1-inf4375/routes/info.js
@@ -1,9 +1,15 @@
 
-
+var jsdom  = require('jsdom');
+var request = require('request');
 
 exports.cours = function(req, res){
 // GET /user/tj
-res.send(req.params[0]);
-// => "tj"
-
-});
+request({
+	  method: 'GET',
+	  headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+     },
+	}, function(error, response, body) {
+	  	res.send(req.params.name);
+	});
+};
