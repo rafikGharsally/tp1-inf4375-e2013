d505b7a781cf6abd9f8aaab2cd7e3be3d5197670
Fred Prieur <fprieur@gmail.com>
2013-06-21 23:49:53 -0400

    ajout du tableau cours et groupe pour le service info

--- tp1-inf4375/routes/info.js
+++ tp1-inf4375/routes/info.js
@@ -25,18 +25,24 @@ exports.cours = function(req, res){
 		var arrayTemp = contenuParser.match(/<TD ALIGN="CENTER" CLASS="paragraphe">([0-9]{3,}).*\n<TD ALIGN="CENTER" CLASS="paragraphe">([0-9]{1,3})/g);
 		var arrayFinal = new Array();
 
+		console.log(arrayTemp);
+
 		if(arrayTemp != null) {
+			arrayFinal.push({'valide': 'oui'});
 			for(var i = 0 ; i < arrayTemp.length ; i++){
 				arrayTemp[i] = arrayTemp[i].replace("<TD ALIGN=\"CENTER\" CLASS=\"paragraphe\">", "");
 				arrayTemp[i] = arrayTemp[i].replace("</TD>", "");
 				arrayTemp[i] = arrayTemp[i].replace("<TD ALIGN=\"CENTER\" CLASS=\"paragraphe\">", "");
-				arrayFinal[i] = arrayTemp[i];
+				arrayFinal.push(
+					{ 
+						'groupe' : arrayTemp[i].split("\n")[0],
+						'place_restantes' : arrayTemp[i].split("\n")[1]
+				});
 			}
-			
-	  	res.send(arrayFinal);
+		res.send(arrayFinal);
 
 		} else {
-			res.send([])
+			res.send([{'valide': 'non'}])
 		}
 	});
 };
